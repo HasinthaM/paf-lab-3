addc55f6c69e1461f358f26f18e4ae4587520299
Hasintha Madushan <hasinthagunawardana2000@gmail.com>
2025-03-01 10:51:24 +0530

    initial commit

---
+++ src/main/java/com/paflab3/sliit_paf/SliitPafApplication.java
@@ -0,0 +1,13 @@
+package com.paflab3.sliit_paf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliitPafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SliitPafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paflab3/sliit_paf/SliitPafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paflab3.sliit_paf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliitPafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
