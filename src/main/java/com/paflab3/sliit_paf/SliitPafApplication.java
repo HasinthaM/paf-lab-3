880bfdf1b9a10a18b922310d590c2469991eb723
Hasintha Madushan <hasinthagunawardana2000@gmail.com>
2025-03-09 21:06:32 +0530

    update

--- src/main/java/com/paflab3/sliit_paf/SliitPafApplication.java
+++ src/main/java/com/paflab3/sliit_paf/SliitPafApplication.java
@@ -3,6 +3,8 @@ package com.paflab3.sliit_paf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -17,6 +19,24 @@ public class SliitPafApplication {
 	public String rootEndpoint() {
 		String message = "Hello world!";
 		return message;
+	}
+
+	@GetMapping("/greet")
+	public String greetEndpoint() {
+		String message = "Welcome to Spring Boot!";
+		return message;
+	}
+
+	@GetMapping("/greet/{name}")
+	public String greetWithNameEndpoint(
+			@PathVariable String name,
+			@RequestParam(required = false) String message) {
+
+		if (message != null && !message.isEmpty()) {
+			return "Hello " + name + "! " + message;
+		} else {
+			return "Hello " + name + "! Welcome to Spring Boot!";
+		}
 	}
 
 }
